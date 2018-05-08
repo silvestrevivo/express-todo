ffa52b8aa1d1d5b73670c5f79f1e8462d8917d4c
silvestrevivo <silvestrevivomillan@gmail.com>
2018-05-08 20:43:18 +0200

    get and post request with dummy data

--- controllers/todoControllers.js
+++ controllers/todoControllers.js
@@ -1,13 +1,26 @@
+const bodyParser = require('body-parser');
+
+var data = [
+  { item: 'get milk' },
+  { item: 'walk dog' },
+  { item: 'listen to music' },
+  { item: 'read a book' }
+];
+
+const urlencodeParser = bodyParser.urlencoded({ extended: false });
+
 module.exports = function (app) {
 
   // getting data from database
   app.get('/todo', (req, res) => {
-    res.render('todo');
+    res.render('todo', { todos: data });
   });
 
   // writting data in database
-  app.post('/todo', (req, res) => {
-
+  app.post('/todo', urlencodeParser, (req, res) => {
+    console.log(req.body)
+    data.push(req.body);
+    res.json(data);
   });
 
   // deleting data in database

6047d1ad7bf10ec2274a8966a0ed8f38b718cb64
silvestrevivo <silvestrevivomillan@gmail.com>
2018-05-08 21:23:05 +0200

    mongoose added with mlab to connect to data base

--- controllers/todoControllers.js
+++ controllers/todoControllers.js
@@ -1,14 +1,26 @@
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  database = require('./database');
 
-var data = [
-  { item: 'get milk' },
-  { item: 'walk dog' },
-  { item: 'listen to music' },
-  { item: 'read a book' }
-];
+// Connect to the database
+mongoose.connect(database.db);
 
+// Create schema
+const todoSchema = new mongoose.Schema({
+  item: String
+});
+
+// Model
+const Todo = mongoose.model('Todo', todoSchema);
+const itemOne = Todo({ item: 'buy flowers' }).save((err) => {
+  if (err) throw err
+  console.log('item saved')
+});
+
+// Parsing the body for post request
 const urlencodeParser = bodyParser.urlencoded({ extended: false });
 
+// end points to serve request on front-end
 module.exports = function (app) {
 
   // getting data from database
